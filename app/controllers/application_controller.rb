63db5efd6e5e5fda7f13ce1d9284fa56c20a4cf1
Gastón Caruso <gstn.caruso@gmail.com>
2020-12-30 07:28:54 -0300

    Setup react

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def index
+    render template: 'layouts/application'
+  end
 end
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -10,14 +10,6 @@ const Hello = props => (
   <div>Hello {props.name}!</div>
 )
 
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Hello name="React" />,
